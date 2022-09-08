cd6ffc16a463a887450293101c11d91c49cd03d5
Nurwahid Muslim <112595135+nurwahidmuslim@users.noreply.github.com>
2022-09-08 10:14:29 +0700

    Add files via upload

---
+++ pertemuan2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main (){
+	//menghitung keliling & luas persegi
+	int a;
+	cout<<"Sisi : " ;
+	cin>>a;
+	
+	cout<<"Hasil"<<endl;
+	cout<<"Keliling = "<<4*a<<endl;
+	cout<<"Luas = "<<a*a;
+	return 0 ;
+}
