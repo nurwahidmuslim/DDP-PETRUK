dc2763a1a2fb62957e232c9ea40eb0a026545817
Nurwahid Muslim <112595135+nurwahidmuslim@users.noreply.github.com>
2022-09-08 10:07:41 +0700

    Add files via upload

---
+++ pertemuan1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string nama,npm,jurusan;
+	
+	cout<<"Nama : ";
+	getline(cin,nama);
+	cout<<"NPM : ";
+	getline(cin,npm);
+	cout<<"Jurusan : ";
+	getline(cin,jurusan);
+	
+	cout<<"Nama : "<<nama<<endl;
+	cout<<"NPM : "<<npm<<endl;
+	cout<<"jurusan : "<<jurusan;
+	
+	return 0;
+}
