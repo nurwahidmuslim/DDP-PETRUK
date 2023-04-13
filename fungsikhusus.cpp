b4674f11d9eb9dab4c6681dd6d7f932686cd795e
Nurwahid Muslim <112595135+nurwahidmuslim@users.noreply.github.com>
2023-04-13 10:54:25 +0700

    Create fungsikhusus.cpp

---
+++ fungsikhusus.cpp
@@ -0,0 +1,14 @@
+//sqrt(x): Mencari akar kuadrat dari suatu bilangan x.
+//pow(x, y): Mencari hasil dari x pangkat y.
+//abs(x): Mengembalikan nilai absolut dari suatu bilangan x (juga dikenal sebagai nilai mutlak).
+//ceil(x): Membulatkan suatu bilangan x ke atas menjadi bilangan integer terdekat.
+//floor(x): Membulatkan suatu bilangan x ke bawah menjadi bilangan integer terdekat.
+//round(x): Membulatkan suatu bilangan x menjadi bilangan integer terdekat.
+//max(x, y): Mengembalikan nilai terbesar dari dua bilangan x dan y.
+//min(x, y): Mengembalikan nilai terkecil dari dua bilangan x dan y.
+//sin(x): Menghitung nilai sin dari suatu sudut dalam radian.
+//cos(x): Menghitung nilai cos dari suatu sudut dalam radian.
+//tan(x): Menghitung nilai tan dari suatu sudut dalam radian.
+//log(x): Menghitung nilai log natural dari suatu bilangan x.
+//exp(x): Menghitung nilai e pangkat x, di mana e adalah konstanta Euler (e = 2.71828...).
+//srand(x): Menginisialisasi generator bilangan acak dengan nilai x sebagai seed.
